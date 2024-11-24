e2720590824097d8875c39f921324816620b3fe8
Altansar <altansar.warez@gmail.com>
2024-11-24 19:34:29 +0100

    Delete licence + change contextmenu

--- contextmenu.bat
+++ contextmenu.bat
@@ -17,9 +17,9 @@ if errorlevel 1 goto install
 :install
 echo Installation du menu contextuel...
 REG ADD HKEY_CURRENT_USER\Software\Classes\%ProgID%\shell\BDNFO /f
-REG ADD HKEY_CURRENT_USER\Software\Classes\%ProgID%\shell\BDNFO /v icon /d "%cd%\test.exe,0" /f
+REG ADD HKEY_CURRENT_USER\Software\Classes\%ProgID%\shell\BDNFO /v icon /d "%cd%\BDNFO.exe,0" /f
 REG ADD HKEY_CURRENT_USER\Software\Classes\%ProgID%\shell\BDNFO\command /f
-REG ADD HKEY_CURRENT_USER\Software\Classes\%ProgID%\shell\BDNFO\command /ve /d "%cd%\test.exe """%%1"""" /f
+REG ADD HKEY_CURRENT_USER\Software\Classes\%ProgID%\shell\BDNFO\command /ve /d "%cd%\BDNFO.exe """%%1"""" /f
 goto end
 
 :remove
